d5c42b713f9a4d8de13d799ef49269f876d2ae33
c9sinner <connorjsheffield@gmail.com>
2024-08-30 10:44:23 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://realists.tech/', // Replace 'your-repo-name' with the actual repository name
+  base: '/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
   build: {
     outDir: 'dist'} // This is the default value
