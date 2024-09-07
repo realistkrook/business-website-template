64548e7b4e4187a254bc32a806dc9101d6edd50e
c9sinner <connorjsheffield@gmail.com>
2024-09-07 16:50:00 +1200

    reconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/realisttech/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  base: '/realiststech/', // Ensure this matches your repository name
+});
