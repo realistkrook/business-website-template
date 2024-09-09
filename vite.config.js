e3c2033bfe53b2c45261dc5352f39c405eaf9b43
c9sinner <connorjsheffield@gmail.com>
2024-09-09 20:06:05 +1200

    please work gang

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Update this if your app is served from a subdirectory
+  base: '/realisttech', // Update this if your app is served from a subdirectory
   plugins: [react()],
   build: {
     outDir: 'dist',
